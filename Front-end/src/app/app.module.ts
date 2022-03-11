07bc26c0dfa2d56e7e75c7da2a9fd2029c9ce559
Abbie Sithole <sithole@thedigitalacademy.co.za>
2022-03-11 12:25:04 +0200

    new project initialization

---
+++ Front-end/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserProfileComponent } from './Components/users-components/user-profile/user-profile.component';
+import { UpdateProfileComponent } from './Components/freelancer-components/update-profile/update-profile.component';
+import { DashboardComponent } from './Components/admin-components/dashboard/dashboard.component';
+import { WelcomeComponent } from './Pages/welcome/welcome.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegisterComponent } from './Pages/register/register.component';
+import { FreelancerDetailsComponent } from './Modals/freelancer-details/freelancer-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserProfileComponent,
+    UpdateProfileComponent,
+    DashboardComponent,
+    WelcomeComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    FreelancerDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
