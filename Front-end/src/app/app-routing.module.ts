03c367caf30e465af18fce6edad163de964dcff1
Mashoto Molefe <mashoto.molefe@dadevs.co.za>
2022-03-22 16:32:01 +0200

    LOGIN AN REGISTER UI Done

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegisterComponent } from './Pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+                        {path: '', component: LoginComponent},
+                        {path:'register', component: RegisterComponent},
+                        {path:'login', component: LoginComponent}
+                      ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
