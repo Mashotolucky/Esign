679ff1bf12ed35cab8a671dd38a17fe27afac545
DA_learner <da_learner@DA-learners-MacBook-Air44.local>
2022-03-28 16:34:48 +0200

    interpreter booking

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InterpreterBookingComponent } from './Components/freelancer-components/interpreter-booking/interpreter-booking.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {path: '', component: InterpreterBookingComponent},
+  {path: 'interpreterbooking', component: InterpreterBookingComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
