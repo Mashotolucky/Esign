c7e3cb8f48c75ebc83fd4de2aed48c78c4932b81
Mashoto Molefe <mashoto.molefe@dadevs.co.za>
2022-03-29 17:31:50 +0200

    Auth register service

--- Front-end/src/app/Models/User.ts
+++ Front-end/src/app/Models/User.ts
@@ -1,7 +1,10 @@
-export class User {
-    firstname!: any;
-    lastname!: any;
-    email!: any;
-    password!: any;
-    confirmPassword!: any;
-}
+export type Role = 'User' | 'Interpretor';
+
+export interface User {
+    id?: number,
+    role?: Role,
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string
+};
--- Front-end/src/app/Services/auth.service.ts
+++ Front-end/src/app/Services/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+import { newUser } from '../Models/newUser';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { User } from '../Models/User';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private httpOptions: {headers: HttpHeaders} = {
+    headers: new HttpHeaders({})
+  }) { }
+
+  register(newUser: newUser): Observable<User> {
+    return this.http
+    .post<User>(`${environment.baseUrl}/auth/register`,
+    newUser,
+    this.httpOptions,
+    )
+    .pipe(take(1));
+  }
 }
+
