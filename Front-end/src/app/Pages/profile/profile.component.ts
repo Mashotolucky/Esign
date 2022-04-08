9f7548846a5a443998a2d380ee2f5f88dd45c2d2
Mashoto Molefe <mashoto.molefe@dadevs.co.za>
2022-04-08 13:20:08 +0200

    create booking done

--- Front-end/src/app/Pages/profile/profile.component.ts
+++ Front-end/src/app/Pages/profile/profile.component.ts
@@ -3,6 +3,9 @@ import { enableRipple } from '@syncfusion/ej2-base';
 import Swal from 'sweetalert2'
 enableRipple(true);
 import { TimePicker } from '@syncfusion/ej2-calendars';
+import { UserService } from 'src/app/Services/user.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BookingService } from 'src/app/Services/booking.service';
 
 
 @Component({
@@ -12,29 +15,56 @@ import { TimePicker } from '@syncfusion/ej2-calendars';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  bookingForm!: FormGroup;
+  intepreterID: any;
+  data: any;
+  constructor(private userService: UserService, private bookingService: BookingService) { }
 
-  
+
 
   ngOnInit(): void {
-  //   let timeObj: TimePicker = new TimePicker({ step:45, placeholder: 'Select a time'
-  // });
-  // timeObj.appendTo('#timepicker');
+    //   let timeObj: TimePicker = new TimePicker({ step:45, placeholder: 'Select a time'
+    // });
+    // timeObj.appendTo('#timepicker');
+    this.bookingForm = new FormGroup({
+      date_: new FormControl('', [Validators.required]),
+      time_: new FormControl('', [Validators.required])
+    });
+
+    this.intepreterID = this.userService.getInterpreterId();
+    sessionStorage.setItem("intepreterID",this.intepreterID);
   }
 
-  simpleAlert(){
+  simpleAlert() {
     Swal.fire({
       // position: 'top-end',
       icon: 'success',
       title: 'Successfully booked',
       showConfirmButton: false,
       timer: 1500,
-       width: '300px'
+      width: '300px'
     })
-    
+
   }
 
 
+  creatBooking(): void {
+    let id = sessionStorage.getItem("intepreterID")
+    this.data = {
+      intepreterID: id,
+      date_: this.bookingForm.value.date_,
+      time_: this.bookingForm.value.time_
+    }
+    console.log(this.data);
 
+    this.bookingService.booking(this.data)
+    .subscribe(res =>{
+      this.simpleAlert();
+    },error =>{
+      console.log(error);
+      
+    })
+    
+  }
 
 }
