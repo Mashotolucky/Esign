2bd08634aebb2d0b9fe298e601eae9d6e94a77ff
Ephraim Tlhako <ephraim.tlhako@dadevs.co.za>
2022-04-12 08:23:52 +0200

    fixing ports

--- Front-end/src/app/Services/booking.service.ts
+++ Front-end/src/app/Services/booking.service.ts
@@ -1,24 +1,26 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingService {
-
-  baseUrl = 'http://localhost:4000/api/v1/';
+  BaseUrl = environment.production? environment.backend+'/users' : environment.devbaseUrl+'/users';
+  
+  //baseUrl = 'http://localhost:4000/api/v1/';
   constructor(private http: HttpClient) { }
 
   booking(data: any,token:any){
     let headers=new HttpHeaders();
     headers=headers.set('Authorization',"Bearer "+token);
-    return this.http.post(this.baseUrl+"users/booking/create",data,{headers});
+    return this.http.post(this.BaseUrl+"/booking/create",data,{headers});
   }
 
   getAllbooking(token: any){
     let headers=new HttpHeaders();
     headers=headers.set('Authorization',"Bearer "+token);
-    return this.http.get(this.baseUrl+"users/booking/interpreter/",{headers})
+    return this.http.get(this.BaseUrl+"/booking/interpreter/",{headers})
   }
 
 
--- Front-end/src/app/Services/login.service.ts
+++ Front-end/src/app/Services/login.service.ts
@@ -1,15 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
-  baseUrl = 'http://localhost:4000/api/v1/auth/login';
+  
+  BaseUrl = environment.production? environment.backend +'/auth/login' : environment.devbaseUrl+'/auth/login';
+  
+ // baseUrl = 'http://localhost:4000/api/v1/auth/login';
   constructor(private http: HttpClient) { }
 
   login(data: any){
-    return this.http.post(this.baseUrl,data);
+    return this.http.post(this.BaseUrl,data);
   }
 }
