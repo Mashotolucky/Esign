0a422cf09cf57ffe72e4b33e7d796a760feec7b6
Ephraim Tlhako <ephraim.tlhako@dadevs.co.za>
2022-04-19 16:28:39 +0200

    vid call client

--- Front-end/src/app/Services/socket.service.ts
+++ Front-end/src/app/Services/socket.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';  
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,29 @@ import { Socket } from 'ngx-socket-io';
 export class SocketService {
 
   constructor(private socket: Socket) { }
+  
+  getMessages(): Observable<any> {
+    return this.socket.fromEvent('message');
+  }
+
+  sendMessage(payload): void {
+    this.socket.emit('send-message', payload);
+  }
+  getIceServer() {
+    return {
+        iceServers: [
+            {
+                urls: ["stun:eu-turn4.xirsys.com"]
+            },
+            {
+                username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
+                credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
+                urls: [
+                    "turn:eu-turn4.xirsys.com:80?transport=udp",
+                    "turn:eu-turn4.xirsys.com:3478?transport=tcp"
+                ]
+            }
+        ]
+    };
+  }
 }
