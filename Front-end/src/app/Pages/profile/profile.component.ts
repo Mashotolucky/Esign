14b6eb943a0a5eee796f94b606b1deadd3f2ba80
Mashoto Molefe <mashoto.molefe@dadevs.co.za>
2022-04-28 12:26:53 +0200

    if slot taken

--- Front-end/src/app/Pages/profile/profile.component.ts
+++ Front-end/src/app/Pages/profile/profile.component.ts
@@ -23,22 +23,22 @@ export class ProfileComponent implements OnInit {
   data: any;
   Interpreter: any;
   userLogged: any;
-  isclientLoggedIn:boolean;
+  isclientLoggedIn: boolean;
 
-  timeArr: any =[];
+  timeArr: any = [];
 
   dateValidator(control: AbstractControl): { [key: string]: boolean } | null {
     if (control?.value) {
-        const today = new Date();
-        const dateToCheck = new Date(control.value);
-        if (dateToCheck < today) {
-            return {'Invalid date': true}
-        }
+      const today = new Date();
+      const dateToCheck = new Date(control.value);
+      if (dateToCheck < today) {
+        return { 'Invalid date': true }
+      }
     }
     return null;
-}
+  }
 
-  constructor(private userService: UserService, private bookingService: BookingService,private activatedRoute:ActivatedRoute, private router: Router, private authService: AuthService) {
+  constructor(private userService: UserService, private bookingService: BookingService, private activatedRoute: ActivatedRoute, private router: Router, private authService: AuthService) {
     // this.Interpreter=this.router.getCurrentNavigation().extras.state?this.router.getCurrentNavigation().extras.state.int:null;
 
   }
@@ -47,59 +47,59 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit(): void {
     let todayDate = Date.now();
-    console.log(todayDate );
-  // this.Interpreter = JSON.parse(localStorage.getItem('user'));
-  this.Interpreter =   JSON.parse(localStorage.getItem("Interpreter"));
-  console.log(this.Interpreter);
-  
+    console.log(todayDate);
+    // this.Interpreter = JSON.parse(localStorage.getItem('user'));
+    this.Interpreter = JSON.parse(localStorage.getItem("Interpreter"));
+    console.log(this.Interpreter);
+
     // this.activatedRoute.params.subscribe(params => {
     //   console.log("obj",params);
     //   this.Interpreter = params;
     //   this.intepreterID = this.Interpreter.id;
-      
+
     //   //this.router.navigate['/clientbooking'];
     // });
 
-   console.log(this.Interpreter.email);
-   
+    console.log(this.Interpreter.email);
+
 
     this.bookingForm = new FormGroup({
-      date_: new FormControl('', [Validators.required,this.dateValidator]),
+      date_: new FormControl('', [Validators.required, this.dateValidator]),
       time_: new FormControl('', [Validators.required])
     });
     this.userLogged = this.authService.getUser();
-   console.log(this.userLogged.role);
+    console.log(this.userLogged.role);
 
-   this.isclientLoggedIn=this.userLogged.role === 'CLIENT'?true:false;
+    this.isclientLoggedIn = this.userLogged.role === 'CLIENT' ? true : false;
 
     this.intepreterID = this.userService.getUser();
-    console.log("interId",this.intepreterID.id)
+    console.log("interId", this.intepreterID.id)
     // console.log(this.intepreterID);
-    localStorage.setItem("intepreterID",this.intepreterID);
+    localStorage.setItem("intepreterID", this.intepreterID);
 
     let token = localStorage.getItem("auth-token");
-    this.bookingService.getAllbookingSlot(token,this.intepreterID.id)
-    .subscribe(res =>{
-      let count = 0;
-      res.forEach(element => {
-        console.log(element);
-        this.timeArr.push(element.time_);
-        if(element.status === null){
-         
-          console.log("there are "+count+" of null");
-          
-        }
-      });
-      
-    })
+    this.bookingService.getAllbookingSlot(token, this.intepreterID.id)
+      .subscribe(res => {
+        let count = 0;
+        res.forEach(element => {
+          console.log(element);
+          this.timeArr.push(element.time_);
+          if (element.status === null) {
+
+            console.log("there are " + count + " of null");
+
+          }
+        });
+
+      })
 
     console.log(this.timeArr);
-    
+
   }
 
 
-  isInterpreter(): boolean{
-    if(this.userLogged.role === 'CLIENT'){
+  isInterpreter(): boolean {
+    if (this.userLogged.role === 'CLIENT') {
       return false
     }
     return true
@@ -117,21 +117,21 @@ export class ProfileComponent implements OnInit {
 
   }
 
- 
+
   async creatBooking() {
     this.activatedRoute.params.subscribe(params => {
       // console.log(params);
       this.Interpreter = params;
       this.intepreterID = this.Interpreter.id;
       console.log(this.intepreterID);
-      
-      this.router.navigate['/clientbooking'];
+
+     
     });
     var id = localStorage.getItem("intepreterID")
     let todayDate = Date.now();
-    console.log(todayDate );
+    console.log(todayDate);
     let timeStamp = timestamp
-    
+
     this.data = {
       intepreterID: parseInt(id),
       date_: this.bookingForm.value.date_,
@@ -141,80 +141,36 @@ export class ProfileComponent implements OnInit {
     console.log(this.data);
 
     let token = localStorage.getItem("auth-token");
-    
-    this.bookingService.getAllbookingSlot(token,id)
-    .subscribe(res =>{
-       res.forEach(element => {
-        console.log(element);
-        
-         if((this.data.date_ === element.date_) && (this.data.time_ === element.time_)){
-          Swal.fire(
-            {
-              icon: 'error',
-              title: "Slot Already taken",
-              showConfirmButton: false,
-              timer: 1900,
-               width: '300px'
-            }
-          ) 
-        }
-        else{
-          this.bookingService.booking(this.data,token)
-          .subscribe(res =>{
+
+      if(this.timeArr.includes(this.data.time_))
+      {
+        Swal.fire(
+              {
+                icon: 'error',
+                title: "Slot Already taken",
+                showConfirmButton: false,
+                timer: 2000,
+                width: '300px'
+              }
+            )
+            this.router.navigate['/profile'];
+      }else{
+        //book
+        this.bookingService.booking(this.data, token)
+          .subscribe(res => {
             this.simpleAlert();
-          },error =>{
+          }, error => {
             console.log(error);
-            
+
           })
-
-        }
-      });
-
-      if(){
-        
       }
-      
-    })
-
+     
     
-
-    // this.activatedRoute.params.subscribe(params => {
-    //   // console.log(params);
-    //   // this.Interpreter = params;
-    //   this.intepreterID = this.Interpreter.id;
-
-    //   localStorage.setItem("intepreterID",this.intepreterID);
-    // });
     this.intepreterID = this.Interpreter.id;
 
-      localStorage.setItem("intepreterID",this.intepreterID);
-  }
-
-
-  bookingSlot(){
-    let token = localStorage.getItem("auth-token");
-
-     this.bookingService.getAllbookingSlot(token,this.intepreterID.id)
-    .subscribe(res =>{
-      res.forEach(element => {
-        if((this.bookingForm.value.date_ === element.date_) && (this.bookingForm.value.time_ === element.time_)){
-          return true
-        }
-        else{
-          return false;
-        }
-      });
-      
-    })
-   
+    localStorage.setItem("intepreterID", this.intepreterID);
   }
 
 }
-// function isInterpreter() {
-//   throw new Error('Function not implemented.');
-// }
 
-// function isInterpreter() {
-//   throw new Error('Function not implemented.');
-// }
 
